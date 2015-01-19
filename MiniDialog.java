6e8b7c97b5019f5c8f86733df4e74836cedbe4f8
Dima Avantaj <javasetraining@gmail.com>
2015-01-19 16:44:36 +0200

    second

---
+++ MiniDialog.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class MiniDialog {
+    public static void main (String[] args) {
+        String nume = JOptionPane.showInputDialog("introdu Numele");
+        String aniStr = JOptionPane.showInputDialog("ce varsta ai(cifre)");
+
+        Scanner kb= new Scanner(System.in);
+
+
+        int ani = Integer.parseInt(aniStr);
+
+        String mesaj= "Salut " + nume + "! Ai " + ani + " si poti merge la ";
+
+        if  (ani < 16 ){
+            mesaj= mesaj+"invatat";
+        }
+        else  {
+            mesaj = mesaj + "film indiiskii";
+        }
+        JOptionPane.showMessageDialog(null, mesaj);
+    }
+}
