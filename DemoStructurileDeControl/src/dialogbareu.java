588fa4e481173df86749ff5af144f71bddcfb579
Dmitry <marvelmd@gmail.com>
2015-01-30 20:46:24 +0200

    begin for GUIform creating

--- DemoStructurileDeControl/src/dialogbareu.java
+++ DemoStructurileDeControl/src/dialogbareu.java
@@ -1,46 +1,44 @@
 import java.util.Scanner;
 
-/**
- * Created by iucosoft7 on 1/30/15.
- */
 public class dialogbareu {
     public static void main(String[] args) {
         System.out.println("arata si ai: ");
 
-        Scanner kb= new Scanner(System.in);
-
-
-
-
-
+        Scanner kb = new Scanner(System.in);
 
 
         int punctaj = Integer.parseInt(kb.nextLine());
 
-        String mesaj= "Tu ai " + punctaj ;
+        String mesaj = "Tu ai introdus punctaj egal " + punctaj + " asta e nota ";
 
 
+        if (punctaj > 100) {
+            mesaj = "nu e adevar! ";
+        } else if (punctaj > 90) {
+            mesaj += "A";
+        } else if (punctaj > 80) {
+            mesaj += "B";
+        } else if (punctaj > 70) {
+            mesaj += "C";
+        } else if (punctaj > 60) {
+            mesaj += "D";
+        } else if (punctaj > 50) {
+            mesaj += "E";
+
+        } else if (punctaj >= 0 && punctaj <= 50) {
+
+            mesaj += "F";
 
 
-        if  (punctaj>90){
-            mesaj += " asta e nota A ";
+        } else if (punctaj <= 0) {
+            mesaj = "nu e adevar! ";
         }
-        else  if (punctaj>80){
-            mesaj += " asta e nota  B ";
-        }
-        else  if (punctaj>70){
-            mesaj += " asta e nota  C ";
-        }
-        else  if (punctaj>60){
-            mesaj += " asta e nota  D ";
-        }
-        else  if (punctaj>50){
-            mesaj += "  asta e nota  E ";
-        }
+
 
 
 
         System.out.println(mesaj);
+
 
     }
 }
